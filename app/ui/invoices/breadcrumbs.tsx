b55348b6978e96d48ee215d22bd06b6be5f562fa
Riya Ghosal <GlitchRemo@users.noreply.github.com>
2023-11-26 14:51:30 +0530

    Adds feature to create an invoice

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -19,7 +19,6 @@ export default function Breadcrumbs({
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
-            aria-current={breadcrumb.active}
             className={clsx(
               breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
             )}
